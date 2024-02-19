6a9998c7193a4747a41fae5c7fd60596ed4daca4
mrp4in <daniel02galicia@gmail.com>
2024-02-19 14:01:45 -0600

    backup before the correction to chatGPT

--- conect.py
+++ conect.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
+import bcrypt
 
 app = Flask(__name__)
 
@@ -23,8 +24,34 @@ def connect_to_db():
     except psycopg2.Error as e:
         print("wARNING- NO CONNECT TO DATA BASE:", e)
     return conn
+# This funtion implemete the insert to information from database
+def authenticate_user(username, password):
+    conn = connect_to_db()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM persona WHERE username = %s  ", (username, ))
+    user = cur.fetchone()
+    cur.close()
+    conn.close()
+    if check_password(password,user[1]):
+        return user
+    else:
+        return None
 
-@app.route('/', methods=['GET', 'POST'])
+# cipharase funtion
+def hash_password(password):
+    # Generar un salt aleatorio
+    salt = bcrypt.gensalt()
+    # Cifrar la contraseña con el salt
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+# funtion validate the information
+def check_password(password, hashed_password):
+    # Verificar si la contraseña coincide con la versión cifrada
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
+
+
+@app.route('/', methods=['GET', 'POST'], endpoint="access")
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -34,6 +61,8 @@ def login():
         else:
             return render_template('index.html', error='Invalid username or password')
     return render_template('index.html')
+
+
 @app.route('/registro',methods=['GET','POST'])
 def vista_registroLogin():
     if request.method == 'POST':
@@ -41,22 +70,16 @@ def vista_registroLogin():
         if conector != None:
             username = request.form['username']
             password = request.form['password']
+            print(len(password))
             cursor = conector.cursor()
-            cursor.execute("INSERT INTO persona (username, password) VALUES (%s, %s)", (username,password))
+            cursor.execute("INSERT INTO persona (username, password) VALUES (%s, %s)", (username,str(hash_password(password))))
             conector.commit()
             cursor.close()
-        return redirect(url_for('/'))
+        return redirect(url_for('access'))
     return render_template('registro.html')
 
 
-def authenticate_user(username, password):
-    conn = connect_to_db()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM persona WHERE username = %s AND password = %s", (username, password))
-    user = cur.fetchone()
-    cur.close()
-    conn.close()
-    return user is not None
+
 
 
 @app.route('/success')
