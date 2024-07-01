c547a3a2f05507d682d7225e3b808860dddb0d16
mrp4in00 <daniel02galicia@gmail.com>
2024-07-01 07:39:12 -0600

    Se han agrego el compose para comunicar la DB y el login

--- conect.py
+++ conect.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
 import bcrypt
+import os
 
 app = Flask(__name__)
 
-# Replace these with your PostgreSQL database credentials
-DB_HOST = 'localhost'
-DB_NAME = 'mylogin'
-DB_USER = 'postgres'
-DB_PASSWORD = 'root'
-DB_PORT ='5432'
+DB_HOST = os.getenv('DB_HOST', 'localhost')
+DB_NAME = os.getenv('DB_NAME', 'mylogin')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'root')
+DB_PORT = os.getenv('DB_PORT', '5432')
 
 def connect_to_db():
     conn = None
@@ -20,38 +20,39 @@ def connect_to_db():
             user=DB_USER,
             password=DB_PASSWORD,
             port=DB_PORT
-    )
+        )
     except psycopg2.Error as e:
-        print("wARNING- NO CONNECT TO DATA BASE:", e)
+        print("WARNING - NO CONNECT TO DATA BASE:", e)
     return conn
-# This funtion implemete the insert to information from database
-def authenticate_user(username, password):
+
+def exist_user(username):
     conn = connect_to_db()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM persona WHERE username = %s  ", (username, ))
+    cur.execute("SELECT * FROM persona WHERE username = %s", (username,))
     user = cur.fetchone()
     cur.close()
     conn.close()
-    if check_password(password,user[1]):
+    if user is not None:
         return user
     else:
         return None
 
-# cipharase funtion
+def authenticate_user(username, password):
+    user = exist_user(username)
+    if user:
+        if check_password(password, user[1]):
+            return user
+    return None
+
 def hash_password(password):
-    # Generar un salt aleatorio
     salt = bcrypt.gensalt()
-    # Cifrar la contraseña con el salt
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
 
-# funtion validate the information
 def check_password(password, hashed_password):
-    # Verificar si la contraseña coincide con la versión cifrada
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
 
-
-@app.route('/', methods=['GET', 'POST'], endpoint="access")
+@app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -62,29 +63,27 @@ def login():
             return render_template('index.html', error='Invalid username or password')
     return render_template('index.html')
 
-
-@app.route('/registro',methods=['GET','POST'])
+@app.route('/registro', methods=['GET', 'POST'])
 def vista_registroLogin():
     if request.method == 'POST':
         conector = connect_to_db()
-        if conector != None:
+        if conector:
             username = request.form['username']
             password = request.form['password']
-            print(len(password))
-            cursor = conector.cursor()
-            cursor.execute("INSERT INTO persona (username, password) VALUES (%s, %s)", (username,str(hash_password(password))))
-            conector.commit()
-            cursor.close()
-        return redirect(url_for('access'))
+            userTMP = exist_user(username)
+            if userTMP is None:
+                cursor = conector.cursor()
+                cursor.execute("INSERT INTO persona (username, password) VALUES (%s, %s)", (username, hash_password(password).decode('utf-8')))
+                conector.commit()
+                cursor.close()
+                return redirect(url_for('login'))
+            else:
+                return render_template('registro.html', errormio='Usuario Existente')
     return render_template('registro.html')
-
-
-
-
 
 @app.route('/success')
 def success():
     return 'Login successful!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
